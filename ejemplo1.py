e4ef061741a780e0e83e2dee594cb03baf567252
A01745134 <a01745134@itesm.mx>
2020-10-29 13:03:28 -0600

    Agregar nuevas funcionalidades y archivo

--- ejemplo1.py
+++ ejemplo1.py
@@ -9,6 +9,13 @@ print(imagen[0][0][0])
 imagen = cv2.resize(imagen,(256,256))
 
 cv2.imwrite("resizeimagen.jpg",imagen) #Escribimos una nueva imagen
-cv2.imshow("image",imagen)
-cv2.waitKey(0) #Espera para que veamos la imagen, presionamos 0 y termina.
+
+imagen = cv2.imread("imagen.jpg")
+imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+print(imagen.shape)
+print(imagen[0][0])
+cv2.imwrite("grayimagen.jpg",imagen)
+
+#cv2.imshow("image",imagen)
+#cv2.waitKey(0) Espera para que veamos la imagen, presionamos 0 y termina.
 
